efb008182317b10d79ef3b3f795392569ab05b16
Greem4 <i@greem4.ru>
2025-02-19 14:36:01 +0300

    project init

---
+++ src/main/java/ru/greemlab/interviewresultsbot/InterviewResultsBotApplication.java
@@ -0,0 +1,13 @@
+package ru.greemlab.interviewresultsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewResultsBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewResultsBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/greemlab/interviewresultsbot/InterviewResultsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.greemlab.interviewresultsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewResultsBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
